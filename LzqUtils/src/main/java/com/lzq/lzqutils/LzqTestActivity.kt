0b55c87899ce02d494111a741dd37088edefcec1
lizhenqi <916064115@qq.com>
2025-09-28 14:55:43 +0800

    更新依赖，删除无用

--- LzqUtils/src/main/java/com/lzq/lzqutils/LzqTestActivity.kt
+++ LzqUtils/src/main/java/com/lzq/lzqutils/LzqTestActivity.kt
@@ -1,20 +1,11 @@
 package com.lzq.lzqutils
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class LzqTestActivity : AppCompatActivity() {
+class LzqTestActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_lzq_test)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.lzq_activity_lzq_test)
     }
 }
--- app/src/main/java/com/lzq/lzqutilscollection/MainActivity.kt
+++ app/src/main/java/com/lzq/lzqutilscollection/MainActivity.kt
@@ -1,49 +1,11 @@
 package com.lzq.lzqutilscollection
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.lzq.lzqutils.CommonUtils
-import com.lzq.lzqutilscollection.ui.theme.LzqUtilsCollectionTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            println(CommonUtils.test())
-            LzqUtilsCollectionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LzqUtilsCollectionTheme {
-        Greeting("Android")
+        setContentView(R.layout.lzq_activity_main)
     }
 }
